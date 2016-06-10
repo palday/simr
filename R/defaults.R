06e153fb7911e9905d139f0b25da3859ee51fa30
Phillip Alday <phillip.alday@unisa.edu.au>
2016-06-10 18:43:51 +0930

    function to extract all predictor names from a model

--- R/defaults.R
+++ R/defaults.R
@@ -10,6 +10,30 @@ getDefaultXname <- function(obj) {
     return(a)
 }
 
+#' @export
+getAllXnames <- function(obj, exclude=c("(Intercept)"), pred.type=c("anova","coef")) {
+    # default to ANOVA style since the default test is LR, whcih does ANOVA-style names
+    # also, coef-style has a lot more problems with multiple comparisons for categorical
+    # regresssors. For continuous regressors, it doesn't make any difference since the n
+    # names are the same either way
+    pred.type <- if(missing(pred.type)) "anova" else match.arg(pred.type)
+
+    if(pred.type == "coef"){
+        if(inherits(obj,"lm")){
+            n <-  names(coef(obj))
+        }else if(inherits(obj,"merMod")){
+            n <- names(fixef(obj))
+        }else{
+            stop("Couldn't automatically determine fixed effect terms for this model.")
+        }
+    }else{
+        # ANOVA-style predictor names
+        n <- attr(terms(obj),"term.labels")
+    }
+
+    n[!(n %in% exclude)]
+}
+
 plotpal <- function(n=length(x), x=getPalette(n)) {
 
     plot(seq_along(x), rep(1, n), col=x, bg=lighten(x), pch=21, cex=10, xlim=c(0, n+1), lwd=3)
